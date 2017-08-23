a6c03054c5e6e1e5254a6ddcc8eef12bb8a8f31d
Aparna Rajpurkar <aparna.arr@gmail.com>
2017-08-23 11:06:24 -0700

    version of scater

--- normalization_attempts/scater.R
+++ normalization_attempts/scater.R
@@ -1,208 +1,252 @@
+###########################################################
+# scater.R
+# Author: Aparna Rajpurkar
+# single cell RNAseq normalization and analysis pipeline
+# Depends on scater, pagoda, scran
+###########################################################
+
+#############
+# libraries #
+#############
+
 library(scater)
-library(knitr)
-library(RUVSeq)
 library(scran)
 library(scde)
 library(org.Mm.eg.db)
-library(gplots)
-library(RBGL)
+library(gplots) # heatmap.2
+library(RBGL) # community detection
+library(matrixStats)
+
+####################
+# user-set globals #
+####################
 
 MAX_CORES = 35
+MIN_READS = 1000000
+MIN_FEATURES = 3000
+MAX_ERCC_PERC = 50
+RUN_NAME = "v3_" # for file name 
 
-args <- c("countfile.tsv",1e-3)
+plotFigs = TRUE
+commDetect = FALSE
+
+#############
+# functions #
+#############
+
+getPCA <- function(sceset, ntop = 500, exprs = "varinfo.ercc") {
+    exprs_mat <- get_exprs(sceset, exprs_value = exprs)
+    rv <- matrixStats::rowVars(exprs_mat)
+    feature_set <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+    exprs_to_plot <- exprs_mat[feature_set,, drop = FALSE]
+    exprs_to_plot <- scale(t(exprs_to_plot), scale = TRUE)
+    keep_feature <- (matrixStats::colVars(exprs_to_plot) > 0.001)
+    keep_feature[is.na(keep_feature)] <- FALSE
+    exprs_to_plot <- exprs_to_plot[, keep_feature]
+    pca <- prcomp(exprs_to_plot)
+    return(pca)
+}
+
+communityDetect<-function(var.cor, sig.cor) {
+    g <- ftM2graphNEL(cbind(var.cor$gene1, var.cor$gene2)[sig.cor,], 
+                          W=NULL, V=NULL, edgemode="undirected")
+    cl <- highlyConnSG(g)$clusters
+    cl <- cl[order(lengths(cl), decreasing=TRUE)]
+    head(cl)
+
+    cl.df<-do.call(rbind, lapply(seq_along(cl), function(i){
+                                 data.frame(CLUSTER=i, cl[[i]])
+    }))
+
+    write.table(cl.df, file=paste0(RUN_NAME, "communities.txt"), quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+    return(cl.df)
+}
+
+#############
+# arguments #
+#############
+
+args <- c("countfile.tsv")
 counts<-read.delim(args[1], header=T)
-fdr_thresh<-args[2]
 
-#rownames(counts)<-sub("(.+)\\..*", "\\1", counts[,1], perl=TRUE)        
+##############
+# clean data #
+##############
+
+# clean ENSEMBL gene names
 rownames(counts)<-counts[,1]
 counts<-counts[,2:ncol(counts)]
 
-#splitCounts <- split(as.data.frame(counts), grepl("ERCC", rownames(counts)))
-#samplesCounts <- splitCounts[["FALSE"]]
-
-#validCells<-colSums(samplesCounts>=20) > 1000
-#samplesCounts<-samplesCounts[,validCells]
-
-ensembl_mouse<-rownames(counts)[grepl("ENSMUSG", rownames(counts))]   
-rownames(counts)<-sub("(ENSMUSG\\d+|ERCC.+)\\.*\\d*", "\\1", rownames(counts), perl=TRUE)        
-
-## Winsorize matrix ##
-#models<-knn.error.models(counts, k=ncol(counts)/4, n.cores = MAX_CORES, min.count.threshold=2, min.nonfailed=5, max.model.plots=10)
-#trim<-3/ncol(counts)
-#
-#fpm <- t((t(log(counts))-models$corr.b)/models$corr.a)
-#tfpm <- winsorize.matrix(fpm, trim)
-#rn <- rownames(counts)
-#cn <- colnames(counts)
-#counts <- round(exp(t(t(tfpm)*models$corr.a+models$corr.b)))
-#counts[counts<0] <- 0
-#rownames(counts) <- rn
-#colnames(counts) <- cn
-#rm(fpm, tfpm)
-#counts <- counts[rowSums(counts) > 0, ] # omit genes without any data after Winsorization
-
-
-#spikeCounts <- splitCounts[["TRUE"]]
-#spikeCounts <- spikeCounts[,validCells]
-
+# get total number of cells from raw counts
 numCells <- ncol(counts)
+
+# set batches
 batch<-c(rep("JUNE5",numCells))
 june29_ids<-grep("June_29", colnames(counts))
 batch[june29_ids]<-"JUNE29"
 
+##########################################################################
+
+# deal with the ENSEMBL gene name issue
+ensembl_mouse<-rownames(counts)[grepl("ENSMUSG", rownames(counts))]   
+rownames(counts)<-sub("(ENSMUSG\\d+|ERCC.+)\\.*\\d*", "\\1", rownames(counts), perl=TRUE)        
+
+# get ercc names from raw counts
+erccs_raw<-rownames(counts[grepl("ERCC",rownames(counts)),])
+
+# set phenotype data (describe cells)
 pd<-data.frame(batch=batch)
 pd<-new('AnnotatedDataFrame', data=pd)
 rownames(pd)<-colnames(counts)
-fd<-data.frame(allgenes=rownames(counts))
-rownames(fd)<-rownames(counts)
-fd<-new('AnnotatedDataFrame', data=fd)
 
-#samplesCounts<-as.matrix(samplesCounts)
-erccs_raw<-rownames(counts[grepl("ERCC",rownames(counts)),])
+# create SCESet to hold all data
 mtecdev<-scater::newSCESet( 
                            countData=counts,
                            phenoData=pd
                            )
 
+# calculate basic QC, setting controls as ERCCs 
 mtecdev<-scater::calculateQCMetrics(
                                     mtecdev,
                                     feature_controls=list(ERCC=erccs_raw)
                                     )
 
 
-filter_by_total_counts <- (mtecdev$total_counts > 1000000)
+# set filters
+filter_by_total_counts <- (mtecdev$total_counts > MIN_READS)
+filter_by_expr_features <- (mtecdev$total_features > MIN_FEATURES)
+filter_by_ERCC <- (mtecdev$pct_counts_feature_controls_ERCC < MAX_ERCC_PERC)
 
-filter_by_expr_features <- (mtecdev$total_features > 3000)
-
-filter_by_ERCC <- (mtecdev$pct_counts_feature_controls_ERCC < 50)
-
+# filter cells
 mtecdev$use <- (
                 filter_by_total_counts &
                 filter_by_expr_features & 
                 filter_by_ERCC
                 )
 
+# view number of cells kept and dropped
 table(mtecdev$use)
 
-mtecdev$use_default <- (
-
-                        !mtecdev$filter_on_total_features &
-                        !mtecdev$filter_on_total_counts &
-                        !mtecdev$filter_on_pct_counts_feature_controls_ERCC &
-                        !mtecdev$is_cell_control
-                )
-table(mtecdev$use_default)
-
+# create very basic gene filter
 filter_genes <- apply(counts(mtecdev[ , pData(mtecdev)$use]), 1, function(x) length(x[x > 5]) >= 2)
 
+# apply filter to data
 fData(mtecdev)$use <- filter_genes
+
+# view number of filtered genes
 table(filter_genes)
 
+# view dimensions of dataset (cells and genes)
 dim(mtecdev[fData(mtecdev)$use, pData(mtecdev)$use])
 
+# calculate log2 raw counts (DO NOT USE FOR ANYTHING OTHER THAN QC)
 set_exprs(mtecdev, "log2_counts") <- log2(counts(mtecdev) + 1)
 
+# create new SCESet with QC'd, filtered data
 mtecdev.qc <- mtecdev[fData(mtecdev)$use, pData(mtecdev)$use]
+
+# get all non-control genes (boolean)
 endog_genes <- !fData(mtecdev.qc)$is_feature_control
+# get all control genes (boolean)
 erccs <- fData(mtecdev.qc)$is_feature_control
 
-#er<-counts(mtecdev.qc[erccs,])
+# get counts for all useable cells and genes
+all.co<-counts(mtecdev.qc)
+# get names for usable cells
+use_cells<-colnames(all.co)
 
-#set.seed(0)
-#knner<-knn.error.models(er, k=ncol(er)/4, n.cores = MAX_CORES, min.count.threshold=2, min.nonfailed=5, max.model.plots=10, min.size.entries=20)
-#varinfoer<-pagoda.varnorm(knner, counts=er, max.adj.var=5, n.cores=MAX_CORES, plot=FALSE)
+#################
+# normalization #
+#################
 
-co<-counts(mtecdev.qc[endog_genes,])
-use_cells<-colnames(co)
+# create a new batch vector for only the usable cells
+# to be used with pagoda.varnorm()
 knnbatch<-c(rep("JUNE5",length(use_cells)))
 june29_ids<-grep("June_29", use_cells)
 knnbatch[june29_ids]<-"JUNE29"
 
-#set.seed(0)
-#knn<-knn.error.models(co, k=ncol(co)/4, n.cores = MAX_CORES, min.count.threshold=2, min.nonfailed=5, max.model.plots=10)
-
-#varinfo<-pagoda.varnorm(knn, counts=co, batch=knnbatch, trim=3/ncol(co), max.adj.var=5, n.cores=MAX_CORES, plot=FALSE)
-#varinfo<-pagoda.varnorm(knn, counts=co, batch=knnbatch, max.adj.var=5, n.cores=MAX_CORES, plot=FALSE)
-#varinfo<-pagoda.subtract.aspect(varinfo, colSums(co[, rownames(knn)]>0))
-
-#combined<-rbind(varinfo$mat, varinfoer$mat)
-
-#exprs_rows <- rownames(get_exprs(mtecdev.qc, exprs_value="exprs"))
-#combined2 <- combined[exprs_rows,,drop=FALSE]
-
-#set_exprs(mtecdev.qc, "varinfo")<-combined2
-
-all.co<-counts(mtecdev.qc)
+# set seed before running knn.error.models
 set.seed(0)
+# get the error model from pagoda: note MAX_CORES
 knn.all<-knn.error.models(all.co, k=ncol(all.co)/4, n.cores = MAX_CORES, min.count.threshold=2, min.nonfailed=5, max.model.plots=10)
+# standardize variance to follow chi-square distribution by ERCC norm
 varinfo.ercc<-pagoda.varnorm(knn.all, counts=all.co, batch=knnbatch, max.adj.var=5, n.cores=MAX_CORES, plot=FALSE, fit.genes=rownames(mtecdev.qc[erccs,]))
 
+# create a new exprs matrix in the SCESet object
 set_exprs(mtecdev.qc, "varinfo.ercc")<-varinfo.ercc$mat
 
-#ruvg <- RUVg(counts(mtecdev.qc), erccs, k = 1)
-#set_exprs(mtecdev.qc, "ruvg1") <- ruvg$normalizedCounts
-#set_exprs(mtecdev.qc, "log2_ruvg1") <- log2(ruvg$normalizedCounts + 1)
-#ruvg <- RUVg(counts(mtecdev.qc), erccs, k = 2)
-#set_exprs(mtecdev.qc, "ruvg2") <- ruvg$normalizedCounts
-#set_exprs(mtecdev.qc, "log2_ruvg2") <- log2(ruvg$normalizedCounts + 1)
+###############
+# PCA results #
+###############
 
-#co<-counts(mtecdev.qc)
+# since plotPCA() from scater doesn't give back an object, 
+# calcuate it separately
+pca<-getPCA(mtecdev.qc, ntop=500, exprs="varinfo.ercc")
 
-#ruvg <- RUVg(co, erccs, k = 1)
-#set_exprs(mtecdev.qc, "ruvg1") <- ruvg$normalizedCounts
-#set_exprs(mtecdev.qc, "log2_ruvg1") <- log2(ruvg$normalizedCounts + 1)
-#ruvg <- RUVg(co, erccs, k = 2)
-#set_exprs(mtecdev.qc, "ruvg2") <- ruvg$normalizedCounts
-#set_exprs(mtecdev.qc, "log2_ruvg2") <- log2(ruvg$normalizedCounts + 1)
+################
+# HVG analysis #
+################
 
-
-#setSpike(mtecdev.qc)<-"ERCC"
-#mtecdev.qc <- computeSpikeFactors(mtecdev.qc, type="ERCC", general.use=FALSE)
-#mtecdev.qc <- computeSumFactors(mtecdev.qc, sizes=seq(20, 80, 5))
-#summary(sizeFactors(mtecdev.qc))
-
-
-#mtecdev.new <- normaliseExprs(
-#                         mtecdev.qc,
-#                         method = "RLE", 
-#                         feature_set = endog_genes
-#                             )
-
-#mtecdev.new<-normalize(mtecdev.qc)
-
-## HVG ANALYSIS ##
+# model variance of data with a loess fit 
 var.fit<-trendVar(mtecdev.qc[endog_genes,], assay="varinfo.ercc", trend="loess", use.spikes=FALSE, span=0.2)
+
+# decompose variance
 var.out<-decomposeVar(mtecdev.qc[endog_genes,], var.fit, assay="varinfo.ercc")
+
+# get HVGs at a cutoff of FDR 5%
 hvg.out<-var.out[which(var.out$FDR <= 0.05 & var.out$bio >= 0.5),]
+
+# order by bio dispersion
 hvg.out<-hvg.out[order(hvg.out$bio, decreasing=TRUE),]
+
+# display number of HVGs
 nrow(hvg.out)
 
-## correlated analysis ##
+#############################
+# gene correlation analysis #
+#############################
+
+# set seed
 set.seed(0)
+# find correlated gene pairs, using HVGs as subset
 var.cor <- correlatePairs(mtecdev.qc, assay="varinfo.ercc", subset.row=rownames(hvg.out))
+
+# see top 10 highly corrleated pairs
 head(var.cor)
+
+# only take highly correlated gene pairs of FDR 5%
 sig.cor <- var.cor$FDR <= 0.05
 
+# find unique gene names of correlated pairs
 chosen <- unique(var.cor$gene1[sig.cor], var.cor$gene2[sig.cor])
+
+# view number of these genes
 length(chosen)
+
+# get varinfo for these gene 
 norm.exprs <- get_exprs(mtecdev.qc, exprs_value="varinfo.ercc")[chosen,,drop=FALSE]
 heat.vals <- norm.exprs - rowMeans(norm.exprs)
 
-g <- ftM2graphNEL(cbind(var.cor$gene1, var.cor$gene2)[sig.cor,], 
-                      W=NULL, V=NULL, edgemode="undirected")
-cl <- highlyConnSG(g)$clusters
-cl <- cl[order(lengths(cl), decreasing=TRUE)]
-head(cl)
+cl <- NULL
+if (commDetect) {
+    cl <- communityDetect(var.cor, sig.cor)
+}
 
-cl.df<-do.call(rbind, lapply(seq_along(cl), function(i){
-                             data.frame(CLUSTER=i, cl[[i]])
-}))
-write.table(cl.df, file="communities.txt", quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
+#########
+# plots #
+#########
 
+stopifnot(plotFigs)
 
-pdf("scater_hist.pdf")
+pdf(paste0(RUN_NAME, "scater.pdf"))
+
+# histogram of raw total counts
 hist(mtecdev$total_counts, breaks=20)
+	
+# histogram of raw total features
 hist(mtecdev$total_features, breaks=20)
+	
+# scatterplot to view raw ERCC percent as a function of total features
 scater::plotPhenoData(
                       mtecdev,
                       aes_string(
@@ -211,28 +255,33 @@ scater::plotPhenoData(
                                  colour = "batch"
                                  )
                       )
-
+	
+# raw count plot of highest-expressed features
 scater::plotQC(mtecdev, type = "highest-expression")
 
+# qc'd PCA plot
 scater::plotPCA(mtecdev.qc[endog_genes, ],
                ntop = 500,
                colour_by = "batch",
                size_by = "total_features",
                exprs_values = "exprs")
 
+# qc'd tSNE plot
 scater::plotTSNE(mtecdev.qc[endog_genes, ],
                  ntop = 500,
-#                 perplexity = 10,
                  colour_by = "batch",
                  size_by = "total_features",
                  exprs_values = "exprs",
                  rand_seed = 0)
+
+# qc'd plot of top contribution of total features to variance in any PCs
 scater::plotQC(mtecdev.qc[endog_genes, ],
                type= "find-pcs",
                variable="total_features",
                exprs_values="exprs"
                )
 
+# qc'd density plot of top likely technical confounders
 scater::plotQC(mtecdev.qc[endog_genes, ],
                type= "expl",
                exprs_values="exprs",
@@ -242,12 +291,15 @@ scater::plotQC(mtecdev.qc[endog_genes, ],
                            "pct_counts_feature_controls_ERCC"
                            )
                )
+
+# post-normalization QC contribution plot
 scater::plotQC(mtecdev.qc[endog_genes, ],
                type= "find-pcs",
                variable="total_features",
                exprs_values="varinfo.ercc"
                )
 
+# post-normalization density plot
 scater::plotQC(mtecdev.qc[endog_genes, ],
                type= "expl",
                exprs_values="varinfo.ercc",
@@ -258,28 +310,32 @@ scater::plotQC(mtecdev.qc[endog_genes, ],
                            )
                )
 
+# post-normalization PCA plot
 scater::plotPCA(mtecdev.qc[endog_genes, ],
                ntop = 500,
                colour_by = "batch",
                size_by = "total_features",
                exprs_values = "varinfo.ercc")
 
+# post-normalization tSNE plot
 scater::plotTSNE(mtecdev.qc[endog_genes, ],
                  ntop = 500,
-#                 perplexity = 10,
                  colour_by = "batch",
                  size_by = "total_features",
                  exprs_values = "varinfo.ercc",
                  rand_seed = 0)
 
-plotExpression(mtecdev.qc, rownames(hvg.out)[1:50]) 
+# violin plots of top highly variable genes
+plotExpression(mtecdev.qc, rownames(hvg.out)[1:25]) 
 
+# PCA plot based on top highly variable genes
 scater::plotPCA(mtecdev.qc[endog_genes, ],
                 feature_set = chosen,
                 colour_by = "batch",
                 size_by = "total_features",
                 exprs_values = "varinfo.ercc")
 
+# tSNE plot based on top highly variable genes
 scater::plotTSNE(mtecdev.qc[endog_genes, ],
                  feature_set = chosen,
                  colour_by = "batch",
@@ -287,130 +343,7 @@ scater::plotTSNE(mtecdev.qc[endog_genes, ],
                  exprs_values = "varinfo.ercc",
                  rand_seed = 0)
 
+# heatmap of top highly variable correlated genes
 heat.out <- heatmap.2(heat.vals, col=bluered, symbreak=TRUE, trace='none', cexRow=0.6)
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "find-pcs",
-#               variable="total_features",
-#               exprs_values="norm_exprs"
-#               )
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "find-pcs",
-#               variable="total_features",
-#               exprs_values="ruvg1"
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "find-pcs",
-#               variable="total_features",
-#               exprs_values="log2_ruvg1"
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "find-pcs",
-#               variable="total_features",
-#               exprs_values="ruvg2"
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "find-pcs",
-#               variable="total_features",
-#               exprs_values="log2_ruvg2"
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "expl",
-#               exprs_values="ruvg1",
-#               variables=c("total_features",
-#                           "total_counts",
-#                           "batch",
-#                           "pct_counts_feature_controls_ERCC"
-#                           )
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "expl",
-#               exprs_values="log2_ruvg1",
-#               variables=c("total_features",
-#                           "total_counts",
-#                           "batch",
-#                           "pct_counts_feature_controls_ERCC"
-#                           )
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "expl",
-#               exprs_values="ruvg2",
-#               variables=c("total_features",
-#                           "total_counts",
-#                           "batch",
-#                           "pct_counts_feature_controls_ERCC"
-#                           )
-#               )
-#
-#scater::plotQC(mtecdev.qc[endog_genes, ],
-#               type= "expl",
-#               exprs_values="log2_ruvg2",
-#               variables=c("total_features",
-#                           "total_counts",
-#                           "batch",
-#                           "pct_counts_feature_controls_ERCC"
-#                           )
-#               )
-#
-#scater::plotPCA(mtecdev.qc[endog_genes, ],
-#               ntop = 500,
-#               colour_by = "batch",
-#               size_by = "total_features",
-#               exprs_values = "ruvg1")
-#
-#scater::plotTSNE(mtecdev.qc[endog_genes, ],
-#                 ntop = 500,
-##                 perplexity = 10,
-#                 colour_by = "batch",
-#                 size_by = "total_features",
-#                 exprs_values = "ruvg1",
-#                 rand_seed = 0)
-#
-#scater::plotPCA(mtecdev.qc[endog_genes, ],
-#               ntop = 500,
-#               colour_by = "batch",
-#               size_by = "total_features",
-#               exprs_values = "log2_ruvg1")
-#
-#scater::plotTSNE(mtecdev.qc[endog_genes, ],
-#                 ntop = 500,
-##                 perplexity = 10,
-#                 colour_by = "batch",
-#                 size_by = "total_features",
-#                 exprs_values = "log2_ruvg1",
-#                 rand_seed = 0)
-#
-#scater::plotPCA(mtecdev.qc[endog_genes, ],
-#               ntop = 500,
-#               colour_by = "batch",
-#               size_by = "total_features",
-#               exprs_values = "ruvg2")
-#
-#scater::plotTSNE(mtecdev.qc[endog_genes, ],
-#                 ntop = 500,
-##                 perplexity = 10,
-#                 colour_by = "batch",
-#                 size_by = "total_features",
-#                 exprs_values = "ruvg2",
-#                 rand_seed = 0)
-#
-#scater::plotPCA(mtecdev.qc[endog_genes, ],
-#               ntop = 500,
-#               colour_by = "batch",
-#               size_by = "total_features",
-#               exprs_values = "log2_ruvg2")
-
-#scater::plotTSNE(mtecdev.qc[endog_genes, ],
-#                 ntop = 500,
-#                 perplexity = 10,
-#                 colour_by = "batch",
-#                 size_by = "total_features",
-#                 exprs_values = "log2_ruvg2",
-#                 rand_seed = 0)
 
 dev.off()
